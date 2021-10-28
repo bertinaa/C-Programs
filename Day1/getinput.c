c00d34e3b81b1d4c78e274fe662bb980a8049796
Bertina S <76837815+bertinaa@users.noreply.github.com>
2021-10-28 12:24:00 +0530

    Create getinput.c

---
+++ Day1/getinput.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1;
+    int num2;
+    int sum;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("%d + %d = %d", num1, num2, sum);
+
+
+
+
+    return 0;
+}
